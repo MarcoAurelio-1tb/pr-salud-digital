35ed5e699f9c8a31989ed636b45cd87d7e26b1e3
Andrea Cortes <joahan.cervantes5043@alumnos.udg.mx>
2024-12-13 11:18:42 -0600

    Actualización Farmacos

--- Farmacologia/js/farmacologia1.js
+++ Farmacologia/js/farmacologia1.js
@@ -3,6 +3,16 @@ const openModal = document.querySelector('.hero_cta');
 const modal = document.querySelector('.modal1');
 const closeModal = document.querySelector('.modal_close1');
 
+const openModal2 = document.querySelector('.hero_cta2');
+const modal2 = document.querySelector('.modal2');
+const closeModal2 = document.querySelector('.modal_close2');
+
+
+const openModal3 = document.querySelector('.hero_cta3');
+const modal3 = document.querySelector('.modal3');
+const closeModal3 = document.querySelector('.modal_close3');
+
+
 
 openModal.addEventListener('click', (e)=>{
     e.preventDefault();
@@ -13,4 +23,34 @@ openModal.addEventListener('click', (e)=>{
 closeModal.addEventListener('click', (e)=>{
     e.preventDefault();
     modal.classList.remove('modal1--show');
+});
+
+
+
+
+
+openModal2.addEventListener('click', (e)=>{
+    e.preventDefault();
+    modal2.classList.add('modal2--show');
+});
+
+
+closeModal2.addEventListener('click', (e)=>{
+    e.preventDefault();
+    modal2.classList.remove('modal2--show');
+});
+
+
+
+
+
+openModal3.addEventListener('click', (e)=>{
+    e.preventDefault();
+    modal3.classList.add('modal3--show');
+});
+
+
+closeModal3.addEventListener('click', (e)=>{
+    e.preventDefault();
+    modal3.classList.remove('modal3--show');
 });
