d75026cdcbfef017a7f40f6456e59e9944d2508f
MarcoAurelio <aurelio.dguez.12@gmail.com>
2025-02-05 12:36:20 -0600

    Salud Mental con imagenes

--- js/condiciones.js
+++ js/condiciones.js
@@ -2,127 +2,91 @@ const contentData = {
   ansiedad: {
     leve: {
       image: "/img/condiciones/ansiedad_leve.png",
-      imageText: "Aún tenemos tiempo para controlar la situación.",
-      description: "Es el nivel más bajo de esta condición",
-      details: "Se asocia a tensiones de la vida diaria, es decir la persona se encuentra en situación de alerta, prestando mayor atención a los estímulos externos. Este estado puede generar complicaciones para la concentración de tareas, al generar una preocupación constante sobre los hechos.",
-      sensations: `- La ansiedad leve puede llegar a controlarse y propicia el aprendizaje y/o a la resolución de problemas.<br>
-                    - Confusión momentánea.<br>
-                    - Sensación de desesperanza ante la incertidumbre.<br>
-                    - Pensamientos que se anclan sobre una situación que es fácil lidiar con ellos.`
+      imageText: "Ansiedad aguda.",
+      description: "",
+      details: "Se asocia a tensiones de la vida diaria causado por los estímulos externos. El estado leve de la ansiedad puede generar problemas de concentración y generar preocupación sobre los hechos. La ansiedad leve puede llegar a controlarse sin intervención de especialistas, además propicia el aprendizaje y/o a la resolución de problemas.",
+      sensations: `Pueden presentarse una ligera sensación de inquietud, tensión muscular ocasional, y molestias estomacales leves. Además, puede incluso fomentar el aprendizaje y la resolución de problemas, ya que impulsa a la persona a manejar situaciones desafiantes.`
     },
     moderado: {
-      image: "/img/condiciones/ansiedad_moderada.png",
-      imageText: "Aun hay tiempo, sería muy recomendable que busques ayuda profesional.",
-      description: "Ansiedad moderada descripción.",
-      details: "Se caracteriza por una mayor respuesta fisiológica, como la tensión muscular, palpitaciones excesivas y sudoración en presencia de eventos estresantes. Al igual que la ansiedad leve, la ansiedad moderada tiende a desarrollar comportamientos que limitan la toma de decisiones. Los individuos con ansiedad moderada están limitados en la percepción de su exterior, centrando sus energías sólo en eventos que le generan angustia. Sin embargo, aún puede reconducir su atención a si se centra en ello.",
-      sensations: `- La ansiedad moderada etc...<br>
-                    - Aumento de pensamientos intrusivos.<br>
-                    - Dificultad para concentrarse.<br>
-                    - Cambios en la motivación o interés, irritabilidad o cambios de humor.`
+      image: "/img/condiciones/ansiedad_mod.png",
+      imageText: "Ansiedad aguda episódica.",
+      description: "",
+      details: "Se caracteriza por una mayor respuesta fisiológica en presencia de eventos estresantes, causando tensión muscular, palpitaciones excesivas y sudoración. La ansiedad moderada tiende a dificultar el pensamiento crítico y dificulta la toma de decisiones, debido a que la percepción de su exterior, lo cual le genera angustia. Sin embargo, aún puede reconducir su atención a si se centra en ello.",
+      sensations: `Palpitaciones ocasionales, opresión en el pecho, respiración acelerada y tensión muscular frecuente. También es común la sudoración moderada y dificultades para conciliar o mantener el sueño.`
     },
     severo: {
-      image: "/img/condiciones/ansiedad_severa.png",
-      imageText: "Debemos tomar medidas ahora mismo, no podemos permitirlo más.",
+      image: "/img/condiciones/ansiedad_sev.png",
+      imageText: "Ansiedad severa.",
       description: "Ansiedad moderada.",
-      details: "Es el grado de ansiedad ya patológico, debido a que se observa un aumento de los síntomas físicos, así como de pensamientos catastróficos aunado a una reducción significativa del campo perceptivo, ya que, la persona no puede concentrarse en otra actividad que no sea la que le genere angustia. Esto puede llevar a la persona al aislamiento.",
-      sensations: `- La ansiedad leve puede llegar a controlarse y propicia al aprendizaje y/o a la resolución de problemas.<br>
-                    - Confusión momentánea.<br>
-                    - Sensación de desesperanza ante la incertidumbre.<br>
-                    - Pensamientos que se anclan sobre una situación que es fácil lidiar con ellos.`
+      details: "Se considera como una patología establecida, debido a que se observa un aumento de síntomas físicos y psicológicos. Comienza una reducción significativa del campo perceptivo, dificulta la capacidad de concentración y realización de actividades diferentes a las que generan ansiedad. Esto puede llevar a la persona al aislamiento.",
+      sensations: `La sensación de angustia o miedo intenso es constante, dificultando la realización de actividades diarias. Los pensamientos catastróficos se vuelven más frecuentes, acompañados de una creciente irritabilidad y una sensación de pérdida de control. Físicamente, se experimentan palpitaciones fuertes, temblores, sudoración excesiva, mareos, náuseas recurrentes y problemas de sueño severos.`
     },
     muySevero: {
-      image: "/img/condiciones/ansiedad_muysevera.png",
+      image: "/img/condiciones/ansiedad_msev.png",
       imageText: "Ansiedad Extremadamente Severa Frase.",
       description: "Ansiedad Extremadamente Severa descripción.",
-      details: "Este nivel de ansiedad es considerado el más grave, debido a que se caracteriza el incremento de los malestares físicos como emocionales (terror).  La persona es incapaz de realizar actividades ajenas a la que le causa angustia. La angustia supone una desorganización de la personalidad. Se manifiesta por un aumento de la actividad motora, disminución de la capacidad para relacionarse, percepción distorsionada y pérdida del pensamiento racional.",
-      sensations: `- La ansiedad extremadamente severa puede llegar a controlarse y propicia al aprendizaje y/o a la resolución de problemas.<br>
-                    - Confusión momentánea.<br>
-                    - Sensación de desesperanza ante la incertidumbre.<br>
-                    - Pensamientos que se anclan sobre una situación que es fácil lidiar con ellos.`
+      details: "Se considera como un estado extremadamente grave de la patología, ya que tanto los síntomas físicos como psicológicos se incrementan. En este estado,la persona es incapaz de realizar otras actividades diferentes a las que le causó el problema. Además puede desencadenar en una desorganización de la personalidad, así como una disminución en la capacidad de relación, percepción, motora y racional.",
+      sensations: `El pánico extremo domina la experiencia, pensamientos intrusivos incontrolables y una sensación de peligro inminente. Episodios de bloqueo mental o desconexión con la realidad. Los síntomas físicos incluyen falta de aire intensa, dolor en el pecho, temblores incontrolables, sudoración extrema, vómitos y crisis de llanto, pudiendo llegar a una parálisis temporal por miedo.`
     }
   },
   estres: {
     leve: {
-      image: "/img/condiciones/ansiedad_leve.png",
-      imageText: "Aún tenemos tiempo para controlar la situación.",
-      description: "ESTRÉS",
-      details: "El estrés cotidiano proviene de exigencias y presiones en períodos cortos, y puede ser un estimulante útil en pequeñas dosis, aunque también puede resultar agotador. Se manifiesta con síntomas como irritabilidad, ansiedad, depresión, dolores de cabeza, dolor de espalda, tensión muscular y problemas gastrointestinales. A pesar de sus efectos, suele ser fácil de manejar.",
-      sensations: `- El estrés leve se puede presentar como enojo o irritabilidad, ansiedad y depresión.<br>
-                    - Dolores de cabeza y espalda tensos.<br>
-                    - Dolor en la mandíbula y tensiones musculares.<br>
-                    - Problemas estomacales e intestinales como acidez, flatulencia, diarrea, estreñimiento.`
+      image: "/img/condiciones/estres_leve.png",
+      imageText: "Estrés agudo.",
+      description: "",
+      details: "Es la respuesta a las situaciones cotidianas de cortos periodos de tiempo, necesario para manejar las situaciones. Esto en ciertas personas, puede ser positivo al ser un estimulante o negativo, agotador, por falta de manejo del mismo y pueden desarrollar algunas actitudes y malestares como:",
+      sensations: `Irritabilidad, ansiedad y depresión, dolores de cabeza, espalda, mandíbula y musculares, etc. A pesar de los síntomas, se considera fácil de manejar.`
     },
     moderado: {
-      image: "/img/condiciones/ansiedad_moderada.png",
-      imageText: "Aun hay tiempo, sería muy recomendable que busques ayuda profesional.",
-      description: "ESTRÉS",
-      details: "ESTRÉS.",
-      sensations: `- ESTRÉS.<br>
-                    - ESTRÉS.<br>
-                    - ESTRÉS.<br>
-                    - ESTRÉS.`
+      image: "/img/condiciones/estres_mod.png",
+      imageText: "Estrés agudo episódico.",
+      description: "",
+      details: "Es un estrés agudo no controlado pero transitorio, se presenta en ante situaciones específicas, generalmente relacionadas con presiones externas. Normalmente lo presentan personas con estilo de vida acelerado, este tipo de estrés puede afectar el bienestar físico y emocional, con síntomas como:",
+      sensations: `Tensión, dolor de cabeza, ansiedad, irritabilidad y preocupaciones constantes. Para lograr manejar el estrés se requiere de ayuda profesional.`
     },
     severo: {
-      image: "/img/condiciones/ansiedad_severa.png",
-      imageText: "Debemos tomar medidas ahora mismo, no podemos permitirlo más.",
-      description: "ESTRÉS",
-      details: "ESTRÉS.",
-      sensations: `- ESTRÉS.<br>
-                    - ESTRÉS.<br>
-                    - ESTRÉS.<br>
-                    - ESTRÉS.`
+      image: "/img/condiciones/estres_sev.png",
+      imageText: "Estrés crónico.",
+      description: "",
+      details: "Es el estrés que persiste durante un largo período de tiempo y está asociado con una exposición constante a situaciones estresantes. El estrés crónico puede tener un impacto significativo en la salud física y emocional ya que desgasta a la persona día tras día. Entre los síntomas más comunes son:",
+      sensations: `Fatiga, tensión, trastorno del sueño, problemas digestivos, enfermedades autoinmunes, ansiedad, depresión e irritabilidad. El diagnóstico es complicado debido a que se normaliza, sin embargo, se requiere ayuda profesional e incluso tratamiento médico.`
     },
     muySevero: {
-      image: "/img/condiciones/ansiedad_muysevera.png",
-      imageText: "Ansiedad Extremadamente Severa Frase.",
-      description: "ESTRÉS",
-      details: "ESTRÉS.",
-      sensations: `- ESTRÉS.<br>
-                    - ESTRÉS.<br>
-                    - ESTRÉS.<br>
-                    - ESTRÉS.`
+      image: "/img/condiciones/estres_msev.png",
+      imageText: "Estrés muy severo.",
+      description: "",
+      details: "Es la última etapa del estrés, se caracteriza por una activación continua del sistema nervioso ante cualquier situación, debido a eventos traumáticos, estrés acumulado o sobrecarga emocional. A diferencia del estrés crónico, este tiene un impacto en la salud física y emocional aún mayor, como:",
+      sensations: `el colapso físico y emocional, ansiedad y depresión severa, pensamientos suicidas, problemas cardíacos e inmunosupresión, entre otros. Debido a que los recursos físicos y mentales se ven consumidos por el desgaste a largo plazo, requiere atención profesional inmediata.`
     }
   },
   depresion: {
     leve: {
-      image: "/img/condiciones/ansiedad_leve.png",
-      imageText: "Aún tenemos tiempo para controlar la situación.",
+      image: "/img/condiciones/depre_leve.png",
+      imageText: "Depresión leve.",
       description: "DEPRESION",
-      details: "Se asocia a tensiones de la vida diaria, es decir la persona se encuentra en situación de alerta, prestando mayor atención a los estímulos externos. Este estado puede generar complicaciones para la concentración de tareas, al generar una preocupación constante sobre los hechos..",
-      sensations: `- La ansiedad leve puede llegar a controlarse y propicia el aprendizaje y/o a la resolución de problemas.<br>
-                      - Confusión momentánea.<br>
-                      - Sensación de desesperanza ante la incertidumbre.<br>
-                      - Pensamientos que se anclan sobre una situación que es fácil lidiar con ellos.`
+      details: `También conocida como distimia, se caracteriza por presentar síntomas depresivos ocasionales y menos intensos. Aunque los síntomas son más suaves, no dejan de ser molestos, aunque no interfiere significativamente en la vida cotidiana. La depresión leve no siempre requiere tratamiento, pero es importante buscar ayuda si los síntomas persisten.`,
+      sensations: "La tristeza, pérdida de interés en actividades que se frecuentaban, falta de apetito e insomnio y fatiga."
     },
     moderado: {
-      image: "/img/condiciones/ansiedad_moderada.png",
-      imageText: "Aun hay tiempo, sería muy recomendable que busques ayuda profesional.",
+      image: "/img/condiciones/depre_moderada.png",
+      imageText: "Depresión moderada.",
       description: "DEPRESION",
-      details: "DEPRESION.",
-      sensations: `- DEPRESION.<br>
-                    - DEPRESION.<br>
-                    - DEPRESION.<br>
-                    - DEPRESION.`
+      details: "La depresión moderada se caracteriza  por presentar síntomas que pueden afectar a sus actividades cotidianas sin llegar a un punto crítico. Los síntomas duran más tiempo (por lo general, al menos dos semanas). La depresión moderada puede beneficiarse de terapia psicológica. Los síntomas más frecuentes son:",
+      sensations: `Sentimientos de culpa o inutilidad, dificultad para concentrarse, cambios en la motivación o interés, irritabilidad o cambios de humor.`
     },
     severo: {
-      image: "/img/condiciones/ansiedad_severa.png",
-      imageText: "Debemos tomar medidas ahora mismo, no podemos permitirlo más.",
+      image: "/img/condiciones/depre_severa.png",
+      imageText: "Depresión severa grave.",
       description: "DEPRESION",
-      details: "DEPRESION.",
-      sensations: `- DEPRESION.<br>
-                    - DEPRESION.<br>
-                    - DEPRESION.<br>
-                    - DEPRESION.`
+      details: `Este nivel es una forma más grave de trastorno depresivo, ya que afecta significativamente la vida de quien la padece. Los síntomas depresivos son más intensos y persistentes. Puede experimentar:`,
+      sensations: `Pensamientos negativos o suicidas, dificultad para realizar tareas diarias, pérdida de interés en actividades que antes disfrutaba, cambios significativos en el apetito o sueño. En este punto es fundamental buscar ayuda profesional de inmediato.`
     },
     muySevero: {
-      image: "/img/condiciones/ansiedad_muysevera.png",
-      imageText: "Ansiedad Extremadamente Severa Frase.",
-      description: "DEPRESION",
-      details: "DEPRESION.",
-      sensations: `- DEPRESION.<br>
-                    - DEPRESION.<br>
-                    - DEPRESION.<br>
-                    - DEPRESION.`
+      image: "/img/condiciones/depre_muysevera.png",
+      imageText: "Depresión muy severa.",
+      description: "Depresión crítica severa.",
+      details: "Es el punto más crítico de la depresión, puede llevar a una pérdida casi total de la capacidad de funcionar en la vida cotidiana, así como también poner en riesgo su vida. Los síntomas que se presentan son extremadamente intensos y debilitantes como:",
+      sensations: `Pensamientos suicidas frecuentes, dificultad para realizar tareas básicas, pérdida total de interés, cambios drásticos en el apetito o sueño. Requieren de ayuda profesional inmediata, por el alto riesgo.`
     }
   }
 };
@@ -149,4 +113,3 @@ function changeContent(level, page, event) {
   document.querySelectorAll(".btn-group .btn").forEach(btn => btn.classList.remove("active"));
   if (event) event.target.classList.add("active");
 }
-  
